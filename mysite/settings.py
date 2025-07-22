fa42225fc2142a82b4d3d7787ca3d02559c7e7ef
sunidhi853 <sunidhijangir1095@gmail.com>
2025-07-22 16:05:48 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#!r*188%p0cpd&k*cdx&sn8xbmhh6+0%62xe^!+l9shchdz)8@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite-jk6y.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
